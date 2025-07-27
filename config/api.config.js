7f3b1304be1872c8a16e87d8dc1d2ed7fee88a2a
Sanjay Choudhary <sanjay.udca@gmail.com>
2025-07-27 16:01:51 +0530

    DTDC create shipment done

--- config/api.config.js
+++ config/api.config.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-    apiBaseUrl: 'https://ship.xpressbees.com/api',    
-    xpressbeesEmail: process.env.XPRESSBEES_EMAIL, 
-    xpressbeesPassword: process.env.XPRESSBEES_PASSWORD, 
+    apiBaseUrl: 'https://ship.xpressbees.com/api',
+    apiDTDCBaseUrl: 'https://alphademodashboardapi.shipsy.io/api',
+    DTDCAPIKEY: 'da66dccac00b76c795e827ffaafd5d',
+    xpressbeesEmail: process.env.XPRESSBEES_EMAIL,
+    xpressbeesPassword: process.env.XPRESSBEES_PASSWORD,
 };
